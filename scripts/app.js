afc8cb5816368a9f2862e81d65b0fcb88685efa1
DobleADev <92317399+DobleADev@users.noreply.github.com>
2024-08-27 18:00:38 -0400

    Agregando un poco mas de feedback para el usuario

--- scripts/app.js
+++ scripts/app.js
@@ -55,9 +55,28 @@ function formatText(text) {
 async function copyOutput() {
     try {
         await navigator.clipboard.writeText(outputElement.innerHTML);
+        selectText("output");
         // console.log('Texto copiado al portapapeles');
     } catch (err) {
         console.error('Error al copiar al portapapeles: ', err);
+    }
+}
+
+function selectText(nodeId) {
+    const node = document.getElementById(nodeId);
+
+    if (document.body.createTextRange) {
+        const range = document.body.createTextRange();
+        range.moveToElementText(node);
+        range.select();
+    } else if (window.getSelection) {
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(node);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    } else {
+        console.warn("Could not select text in node: Unsupported browser.");
     }
 }
 
